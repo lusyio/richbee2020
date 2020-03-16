b2fa2cc5a7bd1993c1ad4bcc0502e1dbd56bd459
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-16 22:08:07 +0300

    открытие кейсов по get

--- template/case.php
+++ template/case.php
@@ -1,5 +1,5 @@
 <div class="case">
     <h2><?=$caseName;?></h2>
     <p><?=$caseService;?></p>
-    <a href="/case.php"><img src="/images/<?=$caseId;?>.png" alt="<?=$caseName;?>" title="<?=$caseName;?>"></a>
+    <a href="/case.php?id=<?=$caseId;?>"><img src="/images/<?=$caseId;?>.png" alt="<?=$caseName;?>" title="<?=$caseName;?>"></a>
 </div>
