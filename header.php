00a5ea4cfc790492936fc3deea0285a818f0d2cd
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-16 20:19:29 +0300

    поправил тень

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ include 'conf.php';
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?ver=1.0.0">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap&subset=cyrillic" rel="stylesheet">
 
     <title>Hello, world!</title>
