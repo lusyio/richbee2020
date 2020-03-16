52ad74f5f80f4b39d87186e0589e5ea03e2df97b
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-16 22:50:05 +0300

    Перенесли отзывы + видео блок

--- template/case/block1.php
+++ template/case/block1.php
@@ -1,9 +1,18 @@
+<?php
+$type = substr($item['img'], -3);
+?>
 <div class="container block1">
-    <p><?=$item['text'];?></p>
-    <?php if(!empty($item['img'])) :?>
-    <div class="position-relative img-block">
-        <img src="/images/case/<?=$item['img'];?>"/>
-        <div class="img-shadow position-absolute"></div>
-    </div>
-    <?php endif;?>
+    <p><?= $item['text']; ?></p>
+    <?php if (!empty($item['img'])) : ?>
+
+        <?php if ($type == 'jpg') : ?>
+            <div class="position-relative img-block">
+                <img src="/images/case/<?= $item['img']; ?>"/>
+                <div class="img-shadow position-absolute"></div>
+            </div>
+        <?php else : ?>
+            video
+        <?php endif; ?>
+
+    <?php endif; ?>
 </div>
