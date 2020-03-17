4c0e9dd50b68deece8789108f8502ad3233a6be4
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-17 17:42:39 +0300

    Каффы все

---
+++ template/case/block3.php
@@ -0,0 +1,26 @@
+<div class="container block3">
+    <p><?= $item['text']; ?></p>
+
+    <div class="d-flex">
+        <div>
+            <div class="position-relative img-block">
+                <img src="/images/case/<?= $item['img1']; ?>"/>
+                <div class="img-shadow position-absolute"></div>
+            </div>
+            <div class="position-relative img-block">
+                <img src="/images/case/<?= $item['img3']; ?>"/>
+                <div class="img-shadow position-absolute"></div>
+            </div>
+        </div>
+        <div>
+            <div class="position-relative img-block">
+                <img src="/images/case/<?= $item['img2']; ?>"/>
+                <div class="img-shadow position-absolute"></div>
+            </div>
+            <div class="position-relative img-block">
+                <img src="/images/case/<?= $item['img4']; ?>"/>
+                <div class="img-shadow position-absolute"></div>
+            </div>
+        </div>
+    </div>
+</div>
