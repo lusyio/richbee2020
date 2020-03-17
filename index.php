6cac191e68a3d56563f7f8d9078d5782bdda9e0a
apanfiloff <panfiloff.work@yandex.ru>
2020-03-17 21:14:36 +0300

    Добавлен каркас услуг для главной

--- index.php
+++ index.php
@@ -8,6 +8,60 @@ $cases = DB('*', 'cases', 'id!=0 Order by id DESC'); ?>
         </div>
     </section>
 
+    <section class="section-services">
+        <div class="container">
+            <div class="section-services__item section-services__item--digital">
+                <h3 class="title">Оцифровка бизнеса</h3>
+                <p></p>
+                <img src="images/general/services-item--digital.png" alt="Оцифровка бизнеса">
+                <a class="btn--learn-more btn--learn-more--inverse" href="#">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Подробнее</span>
+                </a>
+            </div>
+            <div class="section-services__item section-services__item--develop">
+                <h3 class="title">Веб-разработка</h3>
+                <a class="btn--learn-more" href="#">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Подробнее</span>
+                </a>
+            </div>
+            <div class="section-services__item section-services__item--design">
+                <h3 class="title">Проектирование и дизайн</h3>
+                <a class="btn--learn-more" href="#">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Подробнее</span>
+                </a>
+            </div>
+            <div class="section-services__item section-services__item--implement">
+                <h3 class="title">Внедрение и интеграции</h3>
+                <a class="btn--learn-more" href="#">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Подробнее</span>
+                </a>
+            </div>
+            <div class="section-services__item section-services__item--support">
+                <h3 class="title">Сопровождение<br>и поддержка</h3>
+                <a class="btn--learn-more" href="#">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Подробнее</span>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    
+
     <div class="container">
         <?php foreach ($cases as $n) :
             $caseId = $n['id'];
--- services.php
+++ services.php
@@ -8,5 +8,30 @@ include 'header.php';
         </div>
     </section>
 
+    <section class="service__list">
+        <div class="container">
+            <div class="service__item">
+                <div class="service__title">Разработка корпоративных сайтов</div>
+                <div class="service__text"></div>
+            </div>
+            <div class="service__item">
+                <div class="service__title">Разработка интернет-магазинов</div>
+                <div class="service__text"></div>
+            </div>
+            <div class="service__item">
+                <div class="service__title">Разработка сервисов и порталов</div>
+                <div class="service__text">Поэтапная разработка и быстрый старт за 2 месяца</div>
+            </div>
+            <div class="service__item">
+                <div class="service__title">Разработка и внедерение ERP и  CRM -систем</div>
+                <div class="service__text">Оцифровка и автоматизация ваших бизнес-процессов</div>
+            </div>
+            <div class="service__item">
+                <div class="service__title">Техническая поддержка</div>
+                <div class="service__text">Полное сопровождение вашего продукта от контроля работоспособности до доработки</div>
+            </div>
+        </div>
+    </section>
+
 <?php
 include 'footer.php';
