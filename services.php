cb2d137e9024b646e9b60aeae0960869502c2b2f
apanfiloff <panfiloff.work@yandex.ru>
2020-03-18 19:52:50 +0300

    Добавлена анимация для страницы Услуги

--- services.php
+++ services.php
@@ -11,70 +11,70 @@ include 'header.php';
     <section class="service">
         <div class="container">
             <div class="service-block">
-                <h2 class="service-block__title">Оцифровка</h2>
-                <p class="service-block__subtitle">Хватит кормить Билла и вести дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным интерфейсом</p>
+                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Оцифровка</h2>
+                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.5s">Хватит кормить Билла и вести дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным интерфейсом</p>
                 <div class="service-block__list">
                     <div class="service-block__item">
-                        <h3 class="service__title">Разработка ERP-системы</h3>
-                        <p class="service__text">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
-                        <a href="#">Получить КП на разработку ERP</a>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Разработка ERP-системы</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
+                        <a class="wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s" href="#">Получить КП на разработку ERP</a>
                     </div>
                     <div class="service-block__item">
-                        <h3 class="service__title">Разработка CRM-системы</h3>
-                        <p class="service__text">Больше никаких пропущенных заявок и забытых звонков! Каждое обращение клиента будет обработано и назначено менеджеру. Менеджеры будут точно знать кому и когда звонить. а маркетологи - откуда пришла заявка</p>
-                        <a href="#">Получить КП на разработку CRM</a>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Разработка CRM-системы</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Больше никаких пропущенных заявок и забытых звонков! Каждое обращение клиента будет обработано и назначено менеджеру. Менеджеры будут точно знать кому и когда звонить. а маркетологи - откуда пришла заявка</p>
+                        <a class="wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s" href="#">Получить КП на разработку CRM</a>
                     </div>
                 </div>
             </div>
 
             <div class="service-block">
-                <h2 class="service-block__title">Разработка</h2>
-                <p class="service-block__subtitle">Веб-разработка интернет-проектов любой сложности под ключ.<br>2-х месячная гарантия на исправление любых багов после сдачи проекта</p>
+                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Разработка</h2>
+                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.5s">Веб-разработка интернет-проектов любой сложности под ключ.<br>2-х месячная гарантия на исправление любых багов после сдачи проекта</p>
                 <div class="service-block__list">
                     <div class="service-block__item">
-                        <h3 class="service__title">Разработка сервисов и порталов</h3>
-                        <p class="service__text">Разработаем сервис под ваши бизнес-задачи. Получите MVP (минимальную версию продукта) уже через 2 месяца после обращения и протестируйте свои идеи</p>
-                        <a href="#">Получить КП на разработку системы</a>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Разработка сервисов и порталов</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Разработаем сервис под ваши бизнес-задачи. Получите MVP (минимальную версию продукта) уже через 2 месяца после обращения и протестируйте свои идеи</p>
+                        <a class="wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s" href="#">Получить КП на разработку системы</a>
                     </div>
                     <div class="service-block__item">
-                        <h3 class="service__title">Разработка корпоративных сайтов</h3>
-                        <p class="service__text">Получите высококонкурентный современный сайт с уникальным дизайном, готовый к SEO-продвижению. Мы сделаем так, что посетители выбирут вас, а не конкурентов</p>
-                        <a href="#">Получить КП на разработку сайта</a>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Разработка корпоративных сайтов</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Получите высококонкурентный современный сайт с уникальным дизайном, готовый к SEO-продвижению. Мы сделаем так, что посетители выбирут вас, а не конкурентов</p>
+                        <a class="wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s" href="#">Получить КП на разработку сайта</a>
                     </div>
                     <div class="service-block__item">
-                        <h3 class="service__title">Разработка интернет-магазинов</h3>
-                        <p class="service__text">Закажите современный и продающий интернет-магазин с удобной административной панелью и готовый к интеграциям с CRM-системами, складскими программами и онлайн-кассами</p>
-                        <a href="#">Получить КП на разработку интернет-магазина</a>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Разработка интернет-магазинов</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Закажите современный и продающий интернет-магазин с удобной административной панелью и готовый к интеграциям с CRM-системами, складскими программами и онлайн-кассами</p>
+                        <a class="wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s" href="#">Получить КП на разработку интернет-магазина</a>
                     </div>
                 </div>
             </div>
 
             <div class="service-block">
-                <h2 class="service-block__title">Дизайн и проектирование</h2>
-                <p class="service-block__subtitle">Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
+                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Дизайн и проектирование</h2>
+                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.5s">Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
                 <div class="service-block__list">
                     <div class="service-block__item">
-                        <h3 class="service__title">Проектирование систем</h3>
-                        <p class="service__text">Продумаем удобный и интуитивно понятный интерфейс для вашего приложения или системы. Пусть пользователи выполняют бизнес-задачи, вместо того, чтобы путаться в разных кнопках</p>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Проектирование систем</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Продумаем удобный и интуитивно понятный интерфейс для вашего приложения или системы. Пусть пользователи выполняют бизнес-задачи, вместо того, чтобы путаться в разных кнопках</p>
                     </div>
                     <div class="service-block__item">
-                        <h3 class="service__title">Веб-дизайн и графические дизайн</h3>
-                        <p class="service__text">Сочный и яркий или лаконичный и минималистический сайт? Запоминающийся и наполненный смыслом фирменный стиль? Реализуем все задачи под ключ</p>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Веб-дизайн и графические дизайн</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Сочный и яркий или лаконичный и минималистический сайт? Запоминающийся и наполненный смыслом фирменный стиль? Реализуем все задачи под ключ</p>
                     </div>
                 </div>
             </div>
 
             <div class="service-block">
-                <h2 class="service-block__title">Сопровождение и поддержка</h2>
-                <p class="service-block__subtitle">Постоянная поддержка вашего продукта нашимим специалистами. Контроль за работоспособностью и доработка нового функционала</p>
+                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Сопровождение и поддержка</h2>
+                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.5s">Постоянная поддержка вашего продукта нашимим специалистами. Контроль за работоспособностью и доработка нового функционала</p>
                 <div class="service-block__list">
                     <div class="service-block__item">
-                        <h3 class="service__title">Техническая поддрежка</h3>
-                        <p class="service__text">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Техническая поддрежка</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
                     </div>
                     <div class="service-block__item">
-                        <h3 class="service__title">Доработка продукта</h3>
-                        <p class="service__text">Предыдущий подрядичик не завершил проект или не справился с поставленными задачами? Требуется новый функционал для текущей системы? Завершим и доработаем проект в сжатые сроки</p>
+                        <h3 class="service__title wow fadeInUp" data-wow-delay="1s">Доработка продукта</h3>
+                        <p class="service__text wow fadeIn" data-wow-delay="1.5s" data-wow-duration="1.5s">Предыдущий подрядичик не завершил проект или не справился с поставленными задачами? Требуется новый функционал для текущей системы? Завершим и доработаем проект в сжатые сроки</p>
                     </div>
                 </div>
             </div>
