ed6c30d62e47513f32261bdbaaba9b283e7ca05e
apanfiloff <panfiloff.work@yandex.ru>
2020-03-18 17:47:22 +0300

    Добавлена основа страницы Услуги

--- services.php
+++ services.php
@@ -8,27 +8,75 @@ include 'header.php';
         </div>
     </section>
 
-    <section class="service__list">
+    <section class="service">
         <div class="container">
-            <div class="service__item">
-                <div class="service__title">Разработка корпоративных сайтов</div>
-                <div class="service__text"></div>
+            <div class="service-block">
+                <h2 class="service-block__title">Оцифровка</h2>
+                <p class="service-block__subtitle">Хватит кормить Билла и вести дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным интерфейсом</p>
+                <div class="service-block__list">
+                    <div class="service-block__item">
+                        <h3 class="service__title">Разработка ERP-системы</h3>
+                        <p class="service__text">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
+                        <a href="#">Получить КП на разработку ERP</a>
+                    </div>
+                    <div class="service-block__item">
+                        <h3 class="service__title">Разработка CRM-системы</h3>
+                        <p class="service__text">Больше никаких пропущенных заявок и забытых звонков! Каждое обращение клиента будет обработано и назначено менеджеру. Менеджеры будут точно знать кому и когда звонить. а маркетологи - откуда пришла заявка</p>
+                        <a href="#">Получить КП на разработку CRM</a>
+                    </div>
+                </div>
             </div>
-            <div class="service__item">
-                <div class="service__title">Разработка интернет-магазинов</div>
-                <div class="service__text"></div>
+
+            <div class="service-block">
+                <h2 class="service-block__title">Разработка</h2>
+                <p class="service-block__subtitle">Веб-разработка систем любой сложности под ключ.<br>2-х месячная гарантия на исправление любых багов после сдачи проекта</p>
+                <div class="service-block__list">
+                    <div class="service-block__item">
+                        <h3 class="service__title">Разработка сервисов и порталов</h3>
+                        <p class="service__text">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
+                        <a href="#">Получить КП на разработку ERP</a>
+                    </div>
+                    <div class="service-block__item">
+                        <h3 class="service__title">Разработка корпоративных сайтов</h3>
+                        <p class="service__text">Больше никаких пропущенных заявок и забытых звонков! Каждое обращение клиента будет обработано и назначено менеджеру. Менеджеры будут точно знать кому и когда звонить. а маркетологи - откуда пришла заявка</p>
+                        <a href="#">Получить КП на разработку CRM</a>
+                    </div>
+                    <div class="service-block__item">
+                        <h3 class="service__title">Разработка интернет-магазинов</h3>
+                        <p class="service__text">Больше никаких пропущенных заявок и забытых звонков! Каждое обращение клиента будет обработано и назначено менеджеру. Менеджеры будут точно знать кому и когда звонить. а маркетологи - откуда пришла заявка</p>
+                        <a href="#">Получить КП на разработку CRM</a>
+                    </div>
+                </div>
             </div>
-            <div class="service__item">
-                <div class="service__title">Разработка сервисов и порталов</div>
-                <div class="service__text">Поэтапная разработка и быстрый старт за 2 месяца</div>
+
+            <div class="service-block">
+                <h2 class="service-block__title">Дизайн и проектирование</h2>
+                <p class="service-block__subtitle">Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
+                <div class="service-block__list">
+                    <div class="service-block__item">
+                        <h3 class="service__title">Проектирование систем</h3>
+                        <p class="service__text">Продумаем удобный и интуитивно понятный интерфейс для вашего приложения или системы. Пусть пользователи выполняют бизнес-задачи, вместо того, чтобы путаться в разных кнопках</p>
+                    </div>
+                    <div class="service-block__item">
+                        <h3 class="service__title">Веб-дизайн и графические дизайн</h3>
+                        <p class="service__text">Сочный и яркий или лаконичный и минималистический сайт? Запоминающийся и наполненный смыслом фирменный стиль? Реализуем все задачи под ключ</p>
+                    </div>
+                </div>
             </div>
-            <div class="service__item">
-                <div class="service__title">Разработка и внедерение ERP и  CRM -систем</div>
-                <div class="service__text">Оцифровка и автоматизация ваших бизнес-процессов</div>
-            </div>
-            <div class="service__item">
-                <div class="service__title">Техническая поддержка</div>
-                <div class="service__text">Полное сопровождение вашего продукта от контроля работоспособности до доработки</div>
+
+            <div class="service-block">
+                <h2 class="service-block__title">Сопровождение и поддержка</h2>
+                <p class="service-block__subtitle">Постоянная поддержка вашего продукта нашимим специалистами. Контроль за работоспособностью и доработка нового функционала</p>
+                <div class="service-block__list">
+                    <div class="service-block__item">
+                        <h3 class="service__title">Техническая поддрежка</h3>
+                        <p class="service__text">Организуйте взаимодействие между бухгалтерией, складами, производствои и продажами. Оцифруйте все показатели бизнеса и найдите узкие места, мешающие вашей компании расти</p>
+                    </div>
+                    <div class="service-block__item">
+                        <h3 class="service__title">Доработка продукта</h3>
+                        <p class="service__text">Предыдущий подрядичик не завершил проект или не справился с поставленными задачами? Требуется новый функционал для текущей системы? Завершим и доработаем проект в сжатые сроки</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
