f9255d009f96ed8b941bda2d72d7988cacd7ea5b
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-19 13:39:46 +0300

    Шаблон к заполнению кейсов

--- case.php
+++ case.php
@@ -15,17 +15,27 @@ $case = json_decode($caseBody, true);
 //$caseNew = [
 //    [
 //        'type' => 'block1',
-//        'text' => 'Во время разработки сайта каффы.рф (ссылка на кейс) мы ставили перед собой задачу увеличить продажи. Для этого мы занялись seo-продвижением и трафик вырос в 7 раз (ссылка на кейс). Но, трафик трафиком, а конверсия - это другое дело. Было решено поднять привлекательности покупок за счет отзывов с другой площадки.',
-//        'img' => 'kaffy-livemaster.jpg',
+//        'text' => 'Текст',
+//        'img' => 'img.jpg',
 //    ],
 //    [
 //        'type' => 'block2',
-//        'text' => 'Степан Васильев продает свои изделия на сайте Ярмарка Мастеров. Множество людей оставляют отзывы, но на сайт это никак не влияет..почему бы это не исправить, подумали мы? И написали парсер, который соотнес названия товаров на Ярмарке Мастеров и сайте каффы.рф и перенес отзывы.',
+//        'text' => 'Только текст.',
 //    ],
 //    [
-//        'type' => 'block1',
-//        'text' => 'Отзывов получилось так много, что мы решили вывести их в отдельный раздел.',
-//        'img' => 'kaffy-livemaster.mp4',
+//        'type' => 'block3',
+//        'text' => 'Текст к четырем картинкам.',
+//        'img1' => 'img.jpg',
+//        'img2' => 'img.jpg',
+//        'img3' => 'img.jpg',
+//        'img4' => 'img.jpg',
+//    ],
+//    [
+//        'type' => 'block4',
+//        'text' => 'Только текст.',
+//        'img' => 'maket.jpg',
+//        'bg' => 'ffffff',
+//        'color' => '000000',
 //    ],
 //];
 //$newBody = json_encode($caseNew, JSON_UNESCAPED_UNICODE);
@@ -36,25 +46,25 @@ $case = json_decode($caseBody, true);
 ?>
     <div class="section-case" id="caseSection">
         <div class="section-case__circle"></div>
-        
+
         <div class="container">
-                <div class="section-case__info">
-                    <h1 class="section-case__title"><?= $caseName; ?></h1>
-                    <p class="section-case__subtitle"><?= $caseService; ?></p>
-                </div>
-                <div class="section-case__img">
-                    <img src="/images/<?= $caseId; ?>.png" alt="<?= $caseName; ?>" title="<?= $caseName; ?>">
-                </div>
-                    
-                    
+            <div class="section-case__info">
+                <h1 class="section-case__title"><?= $caseName; ?></h1>
+                <p class="section-case__subtitle"><?= $caseService; ?></p>
+            </div>
+            <div class="section-case__img">
+                <img src="/images/<?= $caseId; ?>.png" alt="<?= $caseName; ?>" title="<?= $caseName; ?>">
+            </div>
+
+
         </div>
     </div>
-<?php if(!empty($case)) :?>
-<?php foreach ($case as $item):
-    include 'template/case/'.$item['type'].'.php';
-    ?>
-<?php endforeach; ?>
-<?php endif;?>
+<?php if (!empty($case)) : ?>
+    <?php foreach ($case as $item):
+        include 'template/case/' . $item['type'] . '.php';
+        ?>
+    <?php endforeach; ?>
+<?php endif; ?>
 
 <?php
 include 'footer.php';
