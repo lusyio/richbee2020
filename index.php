bde8946b564f75a895c7edc9152e168c2facc0d6
apanfiloff <panfiloff.work@yandex.ru>
2020-03-19 12:49:05 +0300

    Добавлена анимация на главной в базовом виде

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $cases = DB('*', 'cases', 'id!=0 Order by id DESC'); ?>
 
     <section class="section-services">
         <div class="container">
-            <div class="section-services__item section-services__item--digital">
+            <div class="section-services__item section-services__item--digital wow fadeIn">
                 <div class="section-service__info">
-                    <h3 class="title">Оцифровка бизнеса</h3>
-                    <p>Хватит кормить Билла и вести дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным интерфейсом</p>
+                    <h3 class="title wow fadeInUp">Оцифровка бизнеса</h3>
+                    <p class="wow fadeInUp">Хватит кормить Билла и вести дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным интерфейсом</p>
                 </div>
                 <img src="images/general/services-item--digital.png" alt="Оцифровка бизнеса">
                 <a class="btn--learn-more btn--learn-more--inverse" href="services.php#digital">
@@ -23,10 +23,10 @@ $cases = DB('*', 'cases', 'id!=0 Order by id DESC'); ?>
                     <span class="button-text">Подробнее</span>
                 </a>
             </div>
-            <div class="section-services__item section-services__item--develop">
+            <div class="section-services__item section-services__item--develop wow fadeIn">
                 <div class="section-service__info">
-                    <h3 class="title">Веб-разработка</h3>
-                    <p>Веб-разработка интернет-проектов любой сложности под ключ. 2-х месячная гарантия на исправление любых багов после сдачи проекта</p>
+                    <h3 class="title wow fadeInUp">Веб-разработка</h3>
+                    <p class="wow fadeInUp">Веб-разработка интернет-проектов любой сложности под ключ. 2-х месячная гарантия на исправление любых багов после сдачи проекта</p>
                     
                 </div>
                 <a class="btn--learn-more" href="services.php#develop">
@@ -36,10 +36,10 @@ $cases = DB('*', 'cases', 'id!=0 Order by id DESC'); ?>
                     <span class="button-text">Подробнее</span>
                 </a>
             </div>
-            <div class="section-services__item section-services__item--design">
+            <div class="section-services__item section-services__item--design wow fadeIn">
                 <div class="section-service__info">
-                    <h3 class="title">Проектирование и дизайн</h3>
-                    <p>Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
+                    <h3 class="title wow fadeInUp">Проектирование и дизайн</h3>
+                    <p class="wow fadeInUp">Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
                 </div>
                 <a class="btn--learn-more" href="services.php#design">
                     <span class="circle" aria-hidden="true">
@@ -48,10 +48,10 @@ $cases = DB('*', 'cases', 'id!=0 Order by id DESC'); ?>
                     <span class="button-text">Подробнее</span>
                 </a>
             </div>
-            <div class="section-services__item section-services__item--implement">
+            <div class="section-services__item section-services__item--implement wow fadeIn">
                 <div class="section-service__info">
-                    <h3 class="title">Внедрение и интеграции</h3>
-                    <p>Интеграция готовых решений с доработкой под ваши задачи</p>  
+                    <h3 class="title wow fadeInUp">Внедрение и интеграции</h3>
+                    <p class="wow fadeInUp">Интеграция готовых решений с доработкой под ваши задачи</p>  
                 </div>
                 <a class="btn--learn-more" href="services.php#support">
                     <span class="circle" aria-hidden="true">
@@ -60,10 +60,10 @@ $cases = DB('*', 'cases', 'id!=0 Order by id DESC'); ?>
                     <span class="button-text">Подробнее</span>
                 </a>
             </div>
-            <div class="section-services__item section-services__item--support">
+            <div class="section-services__item section-services__item--support wow fadeIn">
                 <div class="section-service__info">
-                    <h3 class="title">Сопровождение<br>и поддержка</h3>
-                    <p>Постоянная поддержка и доработка вашего продукта</p>
+                    <h3 class="title wow fadeInUp">Сопровождение<br>и поддержка</h3>
+                    <p class="wow fadeInUp">Постоянная поддержка и доработка вашего продукта</p>
                 </div>
                 <a class="btn--learn-more" href="services.php#support">
                     <span class="circle" aria-hidden="true">
--- template/case.php
+++ template/case.php
@@ -1,5 +1,7 @@
-<div class="case">
-    <h2><?=$caseName;?></h2>
-    <p><?=$caseService;?></p>
-    <a href="/case.php?id=<?=$caseId;?>"><img src="/images/<?=$caseId;?>.png" alt="<?=$caseName;?>" title="<?=$caseName;?>"></a>
+<div class="case wow fadeIn">
+    <h2 class="wow fadeInUp"><?=$caseName;?></h2>
+    <p class="wow fadeInUp"><?=$caseService;?></p>
+    <a href="/case.php?id=<?=$caseId;?>">
+        <img class="wow zoomIn" src="/images/<?=$caseId;?>.png" alt="<?=$caseName;?>" title="<?=$caseName;?>">
+    </a>
 </div>
