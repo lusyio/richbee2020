83e714128575cbd348b873570dae1e2958f08915
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-20 11:55:03 +0300

    Блок с отзывом ВК

---
+++ template/case/block5.php
@@ -0,0 +1,16 @@
+<div class="container ">
+    <div class="block5">
+        <div>
+            <p><?= $item['text']; ?></p>
+
+        </div>
+        <div id="vk_post_<?= $item['vk']; ?>"></div>
+        <script type="text/javascript">
+            (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "https://vk.com/js/api/openapi.js?167"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));
+            (function() {
+                if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post('vk_post_<?= $item['vk']; ?>', 136676594, 5673, 'dNliASsKcRQtfHgKiv3qtXp1kf0')) setTimeout(arguments.callee, 50);
+            }());
+        </script>
+
+    </div>
+</div>
