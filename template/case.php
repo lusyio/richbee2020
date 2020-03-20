1676c5824e1177ee099fc5c31ee453824b11abbb
apanfiloff <panfiloff.work@yandex.ru>
2020-03-20 15:32:20 +0300

    Доработана анимация Кейсов

--- template/case.php
+++ template/case.php
@@ -1,9 +1,11 @@
-<a href="/case.php?id=<?= $caseId; ?>" class="case-section__item">
+<div class="case-section__item">
     <div class="case-section__info">
         <h3 class="case__title"><?= $caseName; ?></h3>
         <span class="case__service"><?= $caseService; ?></span>
+        <a href="/case.php?id=<?= $caseId; ?>">Подробнее</a>
     </div>
     <div class="case__img" id="scene">
         <img data-depth="0.2" class="wow zoomIn" src="/images/<?= $caseId; ?>.png" alt="<?= $caseName; ?>" title="<?= $caseName; ?>">
     </div>
-</a>
+    
+</div>
