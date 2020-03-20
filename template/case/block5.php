9b618f5b99796df4e00b73045ed2e766d8103ba9
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-20 13:42:22 +0300

    кейс эльденберт + видео блок

--- template/case/block5.php
+++ template/case/block5.php
@@ -4,13 +4,7 @@
             <p><?= $item['text']; ?></p>
 
         </div>
-        <div id="vk_post_<?= $item['vk']; ?>"></div>
-        <script type="text/javascript">
-            (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "https://vk.com/js/api/openapi.js?167"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));
-            (function() {
-                if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post('vk_post_<?= $item['vk']; ?>', 136676594, 5673, 'dNliASsKcRQtfHgKiv3qtXp1kf0')) setTimeout(arguments.callee, 50);
-            }());
-        </script>
+        <?php include 'vk/'.$item['vk'].'.php'; ?>
 
     </div>
 </div>
