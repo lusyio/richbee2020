cb7d73f7c2d9d48cd8197f775f6a34c9197283b2
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-04-03 16:44:52 +0300

    favicon

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ include 'conf.php';
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="icon" href="/favicon.png" type="image/x-icon">
     <?php include "vendors/styles.php" ?>
 
     <link rel="stylesheet" href="/css/style.css?ver=1.0.2">
