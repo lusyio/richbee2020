bc19b5aad6f4388cd9e3028c500cb77ca32bda7d
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-04-14 18:20:28 +0300

    страница 404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+include 'header.php';
+?>
+
+    <section class="error-page">
+        <div class="container">
+            <h1>Упс, что-то пошло не так :(</h1>
+        </div>
+    </section>
+
+<?php
+include 'footer.php';
