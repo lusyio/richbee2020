a4292b5100884ed5bf9adc39b32d2f671c19fb00
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-04-14 18:25:51 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@ include 'header.php';
 
     <section class="error-page">
         <div class="container">
-            <h1>Упс, что-то пошло не так :(</h1>
+            <h1> Ошибка 404 <br/>Такой страницы не существует</h1>
         </div>
     </section>
 
