e70c44466f484795ef0a2391916f2467f0e1e813
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-04-15 11:08:19 +0300

    версия

--- header.php
+++ header.php
@@ -46,7 +46,7 @@ if (file_exists($pathImgOpenGraph)) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="/css/normalize.min.css">
     <link rel="stylesheet" href="/css/animate.min.css">
-    <link rel="stylesheet" href="/css/style.css?ver=1.0.5">
+    <link rel="stylesheet" href="/css/style.css?ver=1.0.6">
     <link rel="stylesheet" href="/css/lightbox.min.css"/>
     <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap&subset=cyrillic"
           rel="stylesheet">
