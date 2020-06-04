d508a4bf66fdded7d354109115d82cbaa0911d18
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-04 12:28:35 +0300

    Свайпер для услуг

--- template/sections/section-service-cases.php
+++ template/sections/section-service-cases.php
@@ -1,22 +1,67 @@
 <section class="service caseService">
     <div class="container">
         <div class="serviceInfo">
-        <h2 class="service-block__title hidden wow fadeIn">Наши кейсы по этой услуге</h2>
-        <p class="service-block__subtitle hidden wow fadeIn">
-            Ниже представлены лишь некоторые наши работы по услуге <span><?=$serviceName;?></span>. Со всеми работами можно ознакомиться в <a href="/case/" class="link-strike link-strike--thin">нашем портфолио</a>
-        </p>
-      </div>
+            <h2 class="service-block__title hidden wow fadeIn">Наши кейсы по этой услуге</h2>
+            <p class="service-block__subtitle hidden wow fadeIn">
+                Ниже представлены лишь некоторые наши работы по услуге <span><?= $serviceName; ?></span>. Со всеми
+                работами можно ознакомиться в <a href="/case/" class="link-strike link-strike--thin">нашем портфолио</a>
+            </p>
+        </div>
         <!--<span class="case-section__subtitle">Тут подзаголовок на всякий случай</span>-->
         <div class="case-section__list">
             <?php
-
-            foreach ($cases as $item) {
-                $caseFriendlyUrl = $item['friendly_url'];
-                $caseId = $item['id'];
-                $caseName = $item['name'];
-                $caseService = DBOnce('name', 'services', 'id=' . $item['service']);
-                include '../template/case.php';
-            }
+            if (count($cases) > 2) : ?>
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($cases as $item) :
+                            $classItem = '';
+                            $caseId = $item['id'];
+                            $caseFriendlyUrl = $item['friendly_url'];
+                            $caseName = $item['name'];
+                            $caseService = DBOnce('name', 'services', 'id=' . $item['service']); ?>
+                            <div class="swiper-slide">
+                                <?php include '../template/case.php'; ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="case-section-swiper-btn case-section-prev"></div>
+                    <div class="case-section-swiper-btn case-section-next"></div>
+                </div>
+                <script>
+                    const swiper = new Swiper('.swiper-container', {
+                        slidesPerView: 1,
+                        spaceBetween: 15,
+                        loop: true,
+                        navigation: {
+                            nextEl: '.case-section-next',
+                            prevEl: '.case-section-prev',
+                        },
+                        breakpoints: {
+                            574: {
+                                slidesPerView: 1.5,
+                                spaceBetween: 15,
+                            },
+                            768: {
+                                slidesPerView: 2,
+                                spaceBetween: 15,
+                            },
+                            991: {
+                                slidesPerView: 3,
+                                spaceBetween: 15,
+                            },
+                        }
+                    });
+                </script>
+            <?php else: ?>
+                <?php foreach ($cases as $item) :
+                    $classItem = '';
+                    $caseId = $item['id'];
+                    $caseFriendlyUrl = $item['friendly_url'];
+                    $caseName = $item['name'];
+                    $caseService = DBOnce('name', 'services', 'id=' . $item['service']);
+                    include '../template/case.php';
+                endforeach; ?>
+            <?php endif;
             ?>
         </div>
     </div>
