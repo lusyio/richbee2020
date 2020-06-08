38bf7e6ff604ada44e83f0a4b4e29c64a734b90c
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-06-08 13:37:11 +0300

    Страница рассчета стоимости

---
+++ price.php
@@ -0,0 +1,27 @@
+<?php include 'header.php'; ?>
+
+<!-- Marquiz script start -->
+<script src="//script.marquiz.ru/v1.js" type="application/javascript"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        Marquiz.init({
+            id: '5ea820c2a7f82700445013f2',
+            autoOpen: false,
+            autoOpenFreq: 'once',
+            openOnExit: false
+        });
+    });
+</script>
+<!-- Marquiz script end -->
+
+<div class="price">
+    <div class="marquiz__container marquiz__container_inline ">
+        <a class="marquiz__button marquiz__button_blicked marquiz__button_rounded marquiz__button_shadow"
+           href="#popup:marquiz_5ea820c2a7f82700445013f2" data-fixed-side=""
+           data-alpha-color="rgba(0, 71, 255, 0.5)" data-color="#0047FF" data-text-color="#ffffff">Узнать
+            стоимость сайта</a>
+    </div>
+</div>
+
+
+<?php include 'footer.php'; ?>
