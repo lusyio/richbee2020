95331f56640a4640e51884681f2653836c22e24a
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-21 18:27:42 +0300

    создание админки

--- services.php
+++ services.php
@@ -1,73 +1,79 @@
 <?php
 include 'header.php';
 ?>
-<div class="greybg">
+    <div class="greybg">
 
 
-    <section class="service">
-        <div class="container">
-            <div class="service-block" id="digital">
-                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Цифровизация</h2>
-                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.1s">Хватит кормить Билла и вести дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным интерфейсом</p>
+        <section class="service">
+            <div class="container">
+                <div class="service-block" id="digital">
+                    <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Цифровизация</h2>
+                    <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.1s">Хватит кормить Билла и вести
+                        дела в Эксельке! Объедините все бизнес-процессы в единую систему управления с удобным
+                        интерфейсом</p>
 
-<div class="service-block__list">
-<?php
-// erp, crm
-$services = [4,5];
-foreach ($services as $id) {
-  include 'template/service.php';
-}
-?>
-</div>
-
-
-            </div>
-
-            <div class="service-block" id="develop">
-                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Разработка</h2>
-                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.1s">Веб-разработка интернет-проектов любой сложности под ключ.<br>2-х месячная гарантия на исправление любых багов после сдачи проекта</p>
-                <div class="service-block__list">
-                <?php
-                // сайт, им, сервисы
-                $services = [1,2,6];
-                foreach ($services as $id) {
-                  include 'template/service.php';
-                }
-                ?>
-                </div>
-            </div>
-
-            <!-- <div class="service-block" id="design">
-                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Дизайн и проектирование</h2>
-                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.5s">Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
-                <div class="service-block__list">
-                    <div class="service-block__item">
-                        <h3 class="service__title wow fadeInUp" data-wow-delay="0.8s">Проектирование систем</h3>
-                        <p class="service__text wow fadeIn" data-wow-delay="1s" data-wow-duration="2s">Продумаем удобный и интуитивно понятный интерфейс для вашего приложения или системы. Пусть пользователи выполняют бизнес-задачи, вместо того, чтобы путаться в разных кнопках</p>
+                    <div class="service-block__list">
+                        <?php
+                        // erp, crm
+                        $services = [4, 5];
+                        foreach ($services as $id) {
+                            include 'template/service.php';
+                        }
+                        ?>
                     </div>
-                    <div class="service-block__item">
-                        <h3 class="service__title wow fadeInUp" data-wow-delay="0.8s">Веб-дизайн и графические дизайн</h3>
-                        <p class="service__text wow fadeIn" data-wow-delay="1s" data-wow-duration="2s">Сочный и яркий или лаконичный и минималистический сайт? Запоминающийся и наполненный смыслом фирменный стиль? Реализуем все задачи под ключ</p>
+
+
+                </div>
+
+                <div class="service-block" id="develop">
+                    <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Разработка</h2>
+                    <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.1s">Веб-разработка
+                        интернет-проектов любой сложности под ключ.<br>2-х месячная гарантия на исправление любых багов
+                        после сдачи проекта</p>
+                    <div class="service-block__list">
+                        <?php
+                        // сайт, им, сервисы
+                        $services = [1, 2, 6, 8];
+                        foreach ($services as $id) {
+                            include 'template/service.php';
+                        }
+                        ?>
                     </div>
                 </div>
-            </div> -->
 
-            <div class="service-block" id="support">
-                <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Сопровождение и поддержка</h2>
-                <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.1s">Постоянная поддержка вашего продукта нашимим специалистами. Контроль за работоспособностью и доработка нового функционала</p>
-                <div class="service-block__list">
-                <?php
-                // тех.обс, доработка
-                $services = [3,7];
-                foreach ($services as $id) {
-                  include 'template/service.php';
-                }
-                ?>
+                <!-- <div class="service-block" id="design">
+                    <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Дизайн и проектирование</h2>
+                    <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.5s">Продуманный и понятный UI/UX дизайн, который решает задачи бизнеса</p>
+                    <div class="service-block__list">
+                        <div class="service-block__item">
+                            <h3 class="service__title wow fadeInUp" data-wow-delay="0.8s">Проектирование систем</h3>
+                            <p class="service__text wow fadeIn" data-wow-delay="1s" data-wow-duration="2s">Продумаем удобный и интуитивно понятный интерфейс для вашего приложения или системы. Пусть пользователи выполняют бизнес-задачи, вместо того, чтобы путаться в разных кнопках</p>
+                        </div>
+                        <div class="service-block__item">
+                            <h3 class="service__title wow fadeInUp" data-wow-delay="0.8s">Веб-дизайн и графические дизайн</h3>
+                            <p class="service__text wow fadeIn" data-wow-delay="1s" data-wow-duration="2s">Сочный и яркий или лаконичный и минималистический сайт? Запоминающийся и наполненный смыслом фирменный стиль? Реализуем все задачи под ключ</p>
+                        </div>
+                    </div>
+                </div> -->
+
+                <div class="service-block" id="support">
+                    <h2 class="service-block__title wow fadeInUp" data-wow-duration="1s">Сопровождение и поддержка</h2>
+                    <p class="service-block__subtitle wow fadeInUp" data-wow-delay="0.1s">Постоянная поддержка вашего
+                        продукта нашимим специалистами. Контроль за работоспособностью и доработка нового
+                        функционала</p>
+                    <div class="service-block__list">
+                        <?php
+                        // тех.обс, доработка
+                        $services = [3, 7];
+                        foreach ($services as $id) {
+                            include 'template/service.php';
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-</div>
+    </div>
 <?php
 include 'footer.php';
--- template/service-single.php
+++ template/service-single.php
@@ -19,7 +19,7 @@ function advantagesItem($img, $title, $info)
 {
     return '<div class="item">
   <img src="/images/svg/' . $img . '.svg"/>
-  <h3>' . $title . '</h3>
+  <h3>' . $title . '</h 3>
   <p>
   ' . $info . '
   </p>
@@ -42,28 +42,169 @@ function advantagesItem($img, $title, $info)
     </script>
     <!-- Marquiz script end -->
 <?php endif; ?>
-<section class="service service-single"
-         style="padding-bottom:150px;">
-    <div class="container">
-        <div class="service-block position-relative">
-            <h1 class="service-block__title wow fadeIn"><?= $serviceName; ?></h1>
-            <p class="service-block__subtitle wow fadeIn" data-wow-delay="0.2s">
-                <?= $serviceDescription; ?>
-            </p>
-            <?php if ($serviceId == '1' or $serviceId == '2') : ?>
-                <a href="#price" class="btn btn-dark wow fadeIn"
-                   data-wow-delay="0.4s">Рассчитать стоимость</a>
-            <?php else: ?>
-                <a href="/contact/" class="btn btn-dark wow fadeIn" data-wow-delay="0.4s">Рассчитать стоимость</a>
-            <?php endif; ?>
-            <?php if (!empty($cases)) : ?>
-                <a href="#steps"
-                   class="btn btn-outline-dark wow fadeIn"
-                   data-wow-delay="0.7s">Этапы работ</a>
-            <?php endif; ?>
+<?php if ($serviceId == '8') : ?>
+    <section class="service service-account service-single">
+        <div class="container">
+            <h1 class="service-account__title wow fadeIn"><?= $serviceName ?></h1>
+            <p class="service-account__subtitle wow fadeIn" data-wow-delay="0.2s"><?= $serviceDescription; ?></p>
+            <div class="service-account-card">
+                <div class="service-account-card__body">
+                    <p class="service-account-card__title">Оставьте заявку чтобы получить детальный <b>план реализации
+                            проекта</b></p>
+                    <p class="service-account-card__info">Мы свяжемся с вами, чтобы обсудить цели и задачи разработки
+                        личного кабинета и предложим вам план реализации</p>
+                    <form class="service-account-card-form">
+                        <input id="name" type="text" class="service-account-card-form__input" required
+                               placeholder='Как вас зовут?'>
+                        <input id="phone" type="text" class="service-account-card-form__input" required
+                               placeholder='Введите ваш телефон'>
+                        <button type="submit" class="service-account-card-form__button">
+                            Получить план реализации
+                        </button>
+                    </form>
+                    <p class="service-account-card__footer">Нажимая кнопку “Получить план реализации” вы даете свое
+                        согласие на обработку персональных данных и принимаете политику конфиденциальности</p>
+                </div>
+            </div>
+            <img class="service-account__image wow fadeIn" data-wow-delay="0.4s"
+                 src="/images/my-account/MacBook-Pro-16-Silver-Right-Mockup-Free 4.png" alt="mac-book">
         </div>
-    </div>
-</section>
+    </section>
+    <section class="grow-point bg-white">
+        <div class="container">
+            <p class="grow-point__title">Разработка личного кабинета <span>позволит вашему бизнесу вырасти</span> за
+                счет</p>
+            <p class="grow-point__after-title"><b>28</b> точек роста</p>
+
+            <div class="grow-point-card">
+                <div class="grow-point-sub-card-container">
+                    <div class="grow-point-sub-card active">
+                        <div class="grow-point-sub-card__body">
+                            <img src="/images/my-account/Secretary-PNG-Transparent-Image 1.png" alt="">
+                            <p class="grow-point-sub-card__title">Ваши сотрудники</p>
+                        </div>
+                    </div>
+                    <div class="grow-point-sub-card">
+                        <div class="grow-point-sub-card__body">
+                            <img src="/images/my-account/iStock-891451008_1-min 1.png" alt="">
+                            <p class="grow-point-sub-card__title">Ваши клиенты</p>
+                        </div>
+                    </div>
+                    <div class="grow-point-sub-card">
+                        <div class="grow-point-sub-card__body">
+                            <img src="/images/my-account/angry-businessman 1.png" alt="">
+                            <p class="grow-point-sub-card__title">Ваши партнеры</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="grow-point-card__body">
+
+                    <ul class="grow-point-card__ul">
+                        <li class="grow-point-card__li">Видят всю историю работы по каждому клиенту и могут предложить
+                            ему дополнительные услуги или товары
+                        </li>
+                        <li class="grow-point-card__li">Могут вести точечную работу по каждому клиенту, предлагая им
+                            индивидуальные тарифы на услуги
+                        </li>
+                        <li class="grow-point-card__li">Не тратят время на рутинные задачи, такие как составление
+                            отчетов, что повышает эффективное время работы в 2 раза
+                        </li>
+                        <li class="grow-point-card__li">Выставляют счета и формируют закрывающую документацию одним
+                            нажатием кнопки
+                        </li>
+                    </ul>
+                    <ul class="grow-point-card__ul">
+                        <li class="grow-point-card__li">Могут быстро передавать задачи по клиентам в смежные отделы и
+                            видеть информацию по текущим работам
+                        </li>
+                        <li class="grow-point-card__li">Видят всю работу по клиенту и могут оперативно получать от него
+                            необходимую информацию
+                        </li>
+                        <li class="grow-point-card__li">Могут вовремя выставлять заказы поставщикам и оперативно
+                            взаимодействуют с подрядчиками
+                        </li>
+                        <li class="grow-point-card__li">Во время видят и обрабатывают входящие запросы клиентов</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="account-planning bg-white">
+        <div class="container">
+            <p class="account-planning__title">
+                пока конкуренты занимаются устранением бардака и с головой погружены в операционку...
+            </p>
+            <p class="account-planning__after-title">
+                Вы заняты планированием и развитием, потому что <span>бизнес-процессы ведутся без вашего непосредственного
+                участия</span>
+            </p>
+            <div class="account-planning-cards-container">
+                <div class="account-planning-card">
+                    <div class="account-planning-card__body">
+                        <img src="/images/svg/my-account/schedule.svg" alt="schedule">
+                        <p class="account-planning-card__info">
+                            Сотрудники точно знают, что и в какой последовательности делать, <b>тем самым сокращается
+                                расход времени</b> на согласование работы
+                        </p>
+                    </div>
+                </div>
+                <div class="account-planning-card">
+                    <div class="account-planning-card__body">
+                        <img src="/images/svg/my-account/deal.svg" alt="deal">
+                        <p class="account-planning-card__info">
+                            <b>Партнеры остаются с вами навсегда</b>, потому что им легко и просто работать с вашей
+                            компанией
+                        </p>
+                    </div>
+                </div>
+                <div class="account-planning-card">
+                    <div class="account-planning-card__body">
+                        <img src="/images/svg/my-account/website.svg" alt="website">
+                        <p class="account-planning-card__info">
+                            Получаете <b>полную отчетность о всех процессах</b> которые происходят в компании
+                        </p>
+                    </div>
+                </div>
+                <div class="account-planning-card">
+                    <div class="account-planning-card__body">
+                        <img src="/images/svg/my-account/money.svg" alt="money">
+                        <p class="account-planning-card__info">
+                            <b>Прибыль компании растет</b>, потому что клиенты самостоятельно оформляют заказы и
+                            получают необходимую документацию
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <img class="account-planning__img" src="/images/my-account/account-planning.png" alt="">
+            <p class="account-planning__info">В это же время вы</p>
+            <p class="account-planning__text"><span>Занимаетесь</span> тем, что действительно важно - <span>стратегическим планированием и
+                развитием компании</span></p>
+        </div>
+    </section>
+<?php else: ?>
+    <section class="service service-single"
+             style="padding-bottom:150px;">
+        <div class="container">
+            <div class="service-block position-relative">
+                <h1 class="service-block__title wow fadeIn"><?= $serviceName; ?></h1>
+                <p class="service-block__subtitle wow fadeIn" data-wow-delay="0.2s">
+                    <?= $serviceDescription; ?>
+                </p>
+                <?php if ($serviceId == '1' or $serviceId == '2') : ?>
+                    <a href="#price" class="btn btn-dark wow fadeIn"
+                       data-wow-delay="0.4s">Рассчитать стоимость</a>
+                <?php else: ?>
+                    <a href="/contact/" class="btn btn-dark wow fadeIn" data-wow-delay="0.4s">Рассчитать стоимость</a>
+                <?php endif; ?>
+                <?php if (!empty($cases)) : ?>
+                    <a href="#steps"
+                       class="btn btn-outline-dark wow fadeIn"
+                       data-wow-delay="0.7s">Этапы работ</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
 <?php if (file_exists($serviceTemp)) :
 
     include $serviceTemp;
@@ -83,10 +224,11 @@ endif;
         <div class="service">
             <div class="container">
                 <div class="service-block service__steps">
-                    <h2 class="service-block__title-new hidden wow fadeIn" data-wow-duration="1s">Рассчитать примерную <b>стоимость
+                    <h2 class="service-block__title-new hidden wow fadeIn" data-wow-duration="1s">Рассчитать примерную
+                        <b>стоимость
                             разработки сайта</b></h2>
                     <p class="service-block__subtitle hidden wow fadeIn"><b>Ответьте на 5 вопросов</b> и получите
-<b>расчет стоимости</b> разработки вашего cайта</p>
+                        <b>расчет стоимости</b> разработки вашего cайта</p>
                     <div class="marquiz__container marquiz__container_inline">
                         <a class="marquiz__button marquiz__button_blicked marquiz__button_rounded marquiz__button_shadow"
                            href="#popup:marquiz_5ea820c2a7f82700445013f2" data-fixed-side=""
@@ -147,7 +289,8 @@ if (file_exists($serviceTempSteps)) : ?>
                             завершения разработки</p>
                     </div>
                     <div>
-                        <p class="garant__title">Цены и сроки жестко фиксируются в <a href="/docs/Договор_рыба.docx">договоре</a></p>
+                        <p class="garant__title">Цены и сроки жестко фиксируются в <a href="/docs/Договор_рыба.docx">договоре</a>
+                        </p>
                         <p class="garant__text">Никаких скрытых платежей и затягиваний сроков под надуманными
                             предлогами</p>
                     </div>
@@ -221,6 +364,6 @@ if (file_exists($serviceTempSteps)) : ?>
 
 <div class="service-single-main">
     <?php endif; ?>
- 
+
 
 </div>
